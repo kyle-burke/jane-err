736a2ecff785c93b0833d2f0c790401a6c4ef177
Kyle Burke <kyle@massroots.com>
2016-04-25 13:25:50 -0600

    more experimentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jane Err AKA The Pun Also Rises
+
+Eventually this will be a Twitter bot that posts variations on famous book titles.
--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 var spellchecker = require('spellchecker');
+var wordnikApiKey = require('./wordnik');
+var http = require('request-promise');
+var Promise = require('bluebird');
 
-var list = spellchecker.getCorrectionsForMisspelling('wall');
+var originalWord = 'wall';
+var list = getRelatedWords(originalWord);
+var related = pickSimilarWords(originalWord, list, 1);
 
-console.log('word provided: wall');
-console.log('related words: ');
-console.log(list);
+console.log(related);
+
+comparePartsOfSpeech('cat', 'dog')
+.then(function(same) {
+  console.log(same);
+});
+
+function getRelatedWords(input) {
+  return spellchecker.getCorrectionsForMisspelling(input);
+}
+
+function pickSimilarWords(rootWord, words, degreesOfSeparation) {
+  var validWords = [];
+
+  for (var i = 0; i < words.length; i++) {
+    if (rootWord.length !== words[i].length) {
+      continue;
+    }
+
+    var difference = 0;
+    for (var j = 0; j < words[i].length; j++) {
+      if (rootWord[j] !== words[i][j]) {
+        difference++;
+      }
+    }
+
+    if (difference <= degreesOfSeparation && difference !== 0) {
+      validWords.push(words[i]);
+    }
+  }
+
+  return validWords;
+}
+
+function comparePartsOfSpeech(first, second) {
+  var optionsFirst = {
+    uri: 'http://api.wordnik.com:80/v4/word.json/' + first + '/definitions',
+    qs: {
+      api_key: wordnikApiKey
+    }
+  };
+
+  var optionsSecond = {
+    uri: 'http://api.wordnik.com:80/v4/word.json/' + second + '/definitions',
+    qs: {
+      api_key: wordnikApiKey
+    }
+  };
+
+  return Promise.all([http(optionsFirst), http(optionsSecond)])
+  .then(function(response) {
+    firstDefinition = JSON.parse(response[0]);
+    secondDefinition = JSON.parse(response[1]);
+    return firstDefinition[0].partOfSpeech === secondDefinition[0].partOfSpeech;
+  })
+}
